7b5d291c035957e8010b9fc6c800839c707115f5
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-05-04 11:47:18 +0530

    basic file structure

---
+++ frontend/src/Componenets/Pages/MenuePage/MenuePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuePage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MenuePage
---
+++ frontend/src/Componenets/Pages/Promotions/Promotioms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Promotioms = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Promotioms
