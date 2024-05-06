3db74d3688d82069e2bd3d619e54b4bc657f8bcf
Kavindhya Nirmani <kavindhyaliyanage24@gmail.com>
2024-05-06 21:53:51 +0530

    recreate dessert menu using card

--- frontend/src/Data/DessertData.js
+++ frontend/src/Data/DessertData.js
@@ -1,6 +1,10 @@
 // Data.js
-import food1 from '../Assets/1dd.jpg';
-import food2 from '../Assets/2dd.jpg';
+import food1 from '../Assets/1d.jpg';
+import food2 from '../Assets/2d.jpg';
+import food3 from '../Assets/3d.jpg';
+import food4 from '../Assets/4d.jpg';
+import food5 from '../Assets/5d.jpg';
+import food6 from '../Assets/6d.jpg';
 
 
 const List = [
@@ -19,21 +23,21 @@ const List = [
         id: 2
     },
     {
-        image: food1,
+        image: food3,
         name: 'BUCKET/6PC (H&C)',
         description: 'Hot & Crispy Chicken 6 pc Bucket',
         price: 'Rs. 3,140',
         id: 3
     },
     {
-        image: food2,
+        image: food4,
         name: 'Full/8Pc (HC)',
         description: '8 pieces of our Signature crispy, juicy chicken served in a bucket',
         price: 'Rs. 4,270',
         id: 4
     },
     {
-        image: food2,
+        image: food5,
         name: 'Bucket/12 Pc (HC)',
         description: '12 pieces of our Signature crispy, juicy chicken served in a bucket',
         price: 'Rs. 6,220',
@@ -41,7 +45,7 @@ const List = [
     },
 
     {
-        image: food1,
+        image: food6,
         name: 'Quarter/2Pc (HC)',
         description: '2 pieces of our Signature crispy, juicy chicken',
         price: 'Rs. 1,170',
@@ -49,50 +53,7 @@ const List = [
     },
 
 
-    {
-        image: food1,
-        name: 'Quarter/2Pc (HC)',
-        description: '2 pieces of our Signature crispy, juicy chicken',
-        price: 'Rs. 1,170',
-        id: 1
-    },
-    {
-        image: food2,
-        name: 'Half /4Pc (HC)',
-        description: '4 pieces of our Signature crispy, juicy chicken',
-        price: 'Rs. 2,190',
-        id: 2
-    },
-    {
-        image: food1,
-        name: 'BUCKET/6PC (H&C)',
-        description: 'Hot & Crispy Chicken 6 pc Bucket',
-        price: 'Rs. 3,140',
-        id: 3
-    },
-    {
-        image: food2,
-        name: 'Full/8Pc (HC)',
-        description: '8 pieces of our Signature crispy, juicy chicken served in a bucket',
-        price: 'Rs. 4,270',
-        id: 4
-    },
-    {
-        image: food2,
-        name: 'Bucket/12 Pc (HC)',
-        description: '12 pieces of our Signature crispy, juicy chicken served in a bucket',
-        price: 'Rs. 6,220',
-        id: 5
-    },
-
-    {
-        image: food1,
-        name: 'Quarter/2Pc (HC)',
-        description: '2 pieces of our Signature crispy, juicy chicken',
-        price: 'Rs. 1,170',
-        id: 6
-    },
-
+    
     
 ];
 
